4c6aba8f3c73b47531bbcbd96e4968ced1e6dac9
spdu61d5 <imre.balassa@sysdata-pse.net>
2015-09-26 15:22:00 +0200

    fixes

--- src/main/java/com/example/helloworld/InventoryConfiguration.java
+++ src/main/java/com/example/helloworld/InventoryConfiguration.java
@@ -1,10 +1,9 @@
 package com.example.helloworld;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.common.collect.ImmutableMap;
 import io.dropwizard.Configuration;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import io.dropwizard.db.DataSourceFactory;
-import org.hibernate.validator.constraints.NotEmpty;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
@@ -25,32 +24,6 @@ public class InventoryConfiguration extends Configuration {
     }
     public void setDatabase(DataSourceFactory database) {
         this.database = database;
-    }
-
-    @NotEmpty
-    private String template;
-
-    @NotEmpty
-    private String defaultName = "Stranger";
-
-    @JsonProperty
-    public String getTemplate() {
-        return template;
-    }
-
-    @JsonProperty
-    public void setTemplate(String template) {
-        this.template = template;
-    }
-
-    @JsonProperty
-    public String getDefaultName() {
-        return defaultName;
-    }
-
-    @JsonProperty
-    public void setDefaultName(String name) {
-        this.defaultName = name;
     }
 
     @JsonProperty("viewRendererConfiguration")
